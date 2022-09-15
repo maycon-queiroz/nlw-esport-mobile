41e915f9dc83dd5d6defa84ad24e2fac1d329ec0
Maycon Queiroz <fmaycon@gmail.com>
2022-09-14 23:48:13 -0300

    feat(commitlint): add comminlint to improve commit message

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx Let's Go!!!!!!!!!!!</Text>
+      <Text>Open up App.tsx Let's Go Bill!</Text>
       <StatusBar style="auto" />
     </View>
   );
