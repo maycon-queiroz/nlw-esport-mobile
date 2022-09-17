35ff9ddd09f067e2b05d75b835ddc137d3f7ca77
Maycon Queiroz <fmaycon@gmail.com>
2022-09-17 08:57:56 -0300

    feat(Card): add component GameCard

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,48 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ImageBackground,
+  ImageSourcePropType,
+  Text
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { styles } from './styles';
+import { THEME } from '../../theme';
+
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps
+}
+
+export function GameCard({ data, ...res }: Props) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground
+        style={styles.cover}
+        source={data.cover}
+        {...res}
+      >
+
+        <LinearGradient
+          colors={THEME.COLORS.FOOTER}
+          style={styles.footer}
+        >
+
+          <Text style={styles.name}>
+            {data.name}
+          </Text>
+
+          <Text style={styles.ads}>
+            {data.ads} anúncios
+          </Text>
+        </LinearGradient>
+      </ImageBackground >
+    </TouchableOpacity>
+  );
+}
