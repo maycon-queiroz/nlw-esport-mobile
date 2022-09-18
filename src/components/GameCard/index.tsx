f8f2ae1831df4e921724136ad4a41ebbfa43c83b
Maycon Queiroz <fmaycon@gmail.com>
2022-09-17 21:00:50 -0300

    feat(GameCard): update props GameCardProps  to ensure params igual return api

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -2,7 +2,6 @@ import {
   TouchableOpacity,
   TouchableOpacityProps,
   ImageBackground,
-  ImageSourcePropType,
   Text
 } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -11,9 +10,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number
+  }
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -25,7 +26,7 @@ export function GameCard({ data, ...res }: Props) {
     <TouchableOpacity style={styles.container}>
       <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{ uri: data.bannerUrl }}
         {...res}
       >
 
@@ -35,11 +36,11 @@ export function GameCard({ data, ...res }: Props) {
         >
 
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
           </Text>
         </LinearGradient>
       </ImageBackground >
